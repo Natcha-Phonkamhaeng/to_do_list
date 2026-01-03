84e68369ed0f0d99a41706547252601be3b57150
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.5>
2026-01-03 22:33:01 +0700

    draft1

---
+++ main.py
@@ -0,0 +1,42 @@
+# 3 Jan 2026
+
+
+print("------ To Do List ------")
+print("1. Add task")
+print("2. View task")
+print("3. Remove task")
+print("4. Mark as complete")
+print("5. Exit")
+user = int(input("please select items you want to do: "))
+
+def add_task():
+	return print("add_task")
+
+def view_task():
+	pass
+
+def remove_task():
+	pass
+
+def mark_complete():
+	pass
+
+def exit():
+	pass
+	
+if user == 1:
+	add_task()
+elif user == 2:
+	view_task()
+elif user == 3:
+	remove_task()
+elif user == 4:
+	mark_complete()
+elif user == 5:
+	exit()
+
+
+
+
+
+

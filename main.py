92bea4850691460fdeb4afdc798d5ccc5c31dd0b
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.4>
2026-01-22 14:49:28 +0700

    adding view remove and change status task

--- main.py
+++ main.py
@@ -12,37 +12,54 @@ def menu():
 	print("5. Exit program")
 	print("")
 	
-
-def add_task():
+def add_task(): # 1
 	task_name = input("what is your task?: \n")
 	task_status = input("pending or complete ?: \n")
 	task[task_name] = task_status
 
-def view_task():
+def view_task(): # 2
 	print(task)
 
-def remove_task():
-	pass
+def remove_task(): # 3
+	rm_task = input("what task you want to remove?: \n")
 
-def mark_complete():
-	pass
+	if rm_task in task:
+		del task[rm_task]
+	else:
+		print("your task is not in to-do list. Try Again")
+
+
+def mark_complete(): # 4
+	# writing if else condition
+	task_ch = input("what task you want to change status?: \n")
+	if task_ch in task:
+		status_ch = input("what is your new status?: \n")
+		task[task_ch] = status_ch
+	else:
+		print("this task is not in to-do list. Try Again")
 
 
 while True:
 	
 	menu()
-	user = int(input("please select items you want to do: "))
+	
+	try:
+		
+		user = int(input("please select items you want to do: "))
 
-	if user == 1:
-		add_task()
-	elif user == 2:
-		view_task()
-	elif user == 3:
-		remove_task()
-	elif user == 4:
-		mark_complete()
-	elif user == 5: # exit
-		break
+		if user == 1:
+			add_task()
+		elif user == 2:
+			view_task()
+		elif user == 3:
+			remove_task()
+		elif user == 4:
+			mark_complete()
+		elif user == 5: # exit
+			break
+
+	except ValueError:
+		print("please input items as numeric only")
 
 
 
